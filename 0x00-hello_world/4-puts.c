afd6de3715fddcc2a25efc586384c30a6c9176fd
hady444 <hadyhassan2312@gmail.com.com>
2023-07-13 12:25:31 +0000

    cm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main is the function that program first look at
- * return 0 Success
+ * main - print the word
+ * Return 0 (Success)
  */
-
 int main(void)
 {
 	/* print the word */
