5580148ac23bb8b6aae603cea73d52beb1e94f49
hady444 <hadyhassan2312@gmail.com.com>
2023-07-13 13:16:42 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 /**
- * main - print size of some types
+ * main - print size of
  * Return: 0 (Success)
  */
 int main(void)
 {
-	char *arr[] = {"char", "int", "long int", "long long int", "float"};
-	int i;
-	for (i=0;i<5;i++){
-		printf("Size of a %s: %d byte(s)\n", arr[i],(unsigned long) sizeof(arr[i]));
-		/*printf("Size of an int: %d byte(s)\n", (unsigned long) sizeof(int));
-		printf("Size of a long int: %d byte(s)\n", (unsigned long) sizeof(long int));
-		printf("Size of a long long int: %d byte(s)\n", (unsigned long) sizeof(long long int));
-		printf("Size of a float: %d byte(s)\n", (unsigned long) sizeof(float));
-		*/
+	
+	printf("Size of a char: %d byte(s)\n",(unsigned long) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (unsigned long) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (unsigned long) sizeof(float));
 	}
 	return (0);
 }
