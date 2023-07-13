7db5f7327ff302e32c291937857c3df1825904f8
hady444 <hadyhassan2312@gmail.com.com>
2023-07-13 13:09:41 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,8 @@
 int main(void)
 {
 	char *arr[] = {"char", "int", "long int", "long long int", "float"};
-	for (int i=0;i<5;i++){
+	int i;
+	for (i=0;i<5;i++){
 		printf("Size of a %s: %d byte(s)\n", (unsigned long) sizeof(%arr[i]));
 		/*printf("Size of an int: %d byte(s)\n", (unsigned long) sizeof(int));
 		printf("Size of a long int: %d byte(s)\n", (unsigned long) sizeof(long int));
