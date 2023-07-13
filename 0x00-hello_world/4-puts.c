58b869c8de137dcb34608052c321f3fd0421f067
hady444 <hadyhassan2312@gmail.com.com>
2023-07-13 12:22:19 +0000

    cm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main is the function that program first look at
+ * return 0 Success
  */
 
 int main(void)
 {
+	/* print the word */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
