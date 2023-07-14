b22174e75dc1c5bbc65cfb4f144cb16f9c6b277c
hady444 <hadyhassan2312@gmail.com.com>
2023-07-14 07:42:39 +0000

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *main - easy steps
+ *
+ *Description: numbers
+ *
+ *Return: 0 Success
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
