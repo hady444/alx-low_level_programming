8c2482ea00fcd3da3a6a60ed36e2d5f72b92b566
hady444 <hadyhassan2312@gmail.com.com>
2023-07-14 07:23:06 +0000

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *main - easy steps
+ *
+ *Description: get lower
+ *
+ *Return: 0 Success
+*/
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ *main - easy steps
+ *
+ *Description: get lower
+ *
+ *Return: 0 Success
+*/
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha != 'Z' + 1)
+	{
+		putchar(alpha);
+		if (alpha == 'z')
+		{
+			alpha = 'A';
+		}
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
