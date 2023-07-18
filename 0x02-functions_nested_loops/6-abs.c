f7f201434cf46c4de8f95f99351fba973fa0a641
hady444 <hadyhassan2312@gmail.com.com>
2023-07-18 20:16:48 +0000

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - kndf
+ * Description: fsv
+ * Return: mffs
+ *
+*/
+int _abs(int)
+{
+	if (int < 0)
+		printf("%d",int * -1);
+	else
+		printf("%d",int);
+	return (0);
+}
