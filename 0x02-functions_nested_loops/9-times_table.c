efa628f11779ce9fe617646fe82f7850402bf7df
hady444 <hadyhassan2312@gmail.com.com>
2023-07-18 21:41:50 +0000

    c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,12 @@ void times_table(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 9 ; j++)
-			printf("%2d, ", i * j);
-		printf("% 2d\n", i * 9);
+		{
+			if (j == 0)
+				printf("%2d", i * j);
+			else
+				printf(", %2d", i * j);
+		}
+		printf("\n")
 	}
 }
