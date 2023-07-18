af3b56bbb8a428f45cdb592e2582dc5bdf669614
hady444 <hadyhassan2312@gmail.com.com>
2023-07-18 21:17:45 +0000

    c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * jack_bauer - klnsf
+ * Description: klmfs
+ * Return: mksfmg
+*/
+void jack_bauer(void)
+{
+	int hour, minute;
+	for (hour = 0 ; hour < 24 ; hour++)
+	{
+		for (minute=0 ; minute < 60 ; minute++)
+		{
+			printf("%02d:%02d", hour, minute)
+		}
+	}
+}
