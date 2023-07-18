8b0307af1edce904d7578582a956f468814fa62a
hady444 <hadyhassan2312@gmail.com.com>
2023-07-18 18:53:13 +0000

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - fgf
+ * Description: gkfg
+ * Return: 0 Success
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,17 @@
+#include <unistid.h>
+#include "main.h"
+/**
+ * print_alphabet - print all alphabets
+ * Description: all lowercase
+ *
+ * Return: void no value returned
+*/
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+		putchar(i);
+	putchar('\n');
+}
