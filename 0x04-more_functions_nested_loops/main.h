cbe294b13bde1eb3487b306320e46367d5e00942
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 14:58:56 +0000

    cc

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,7 +10,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void prvoid print_square(int size);
-int_diagonal(int n);
+void print_diagonal(int n);
+void print_square(int size);
 
 #endif /* MAIN_H */
