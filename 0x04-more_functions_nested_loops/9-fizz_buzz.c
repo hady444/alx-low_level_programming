53cc17af5d332ca53542d86633202637911a7f70
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 15:09:45 +0000

    cc

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ void fizz_buzz(void)
 	printf("1");
 	for (i = 2 ; i <= 100 ; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (((i % 3 == 0)) && ((i % 5) == 0))
 			printf(" FizzBuzz");
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 			printf(" Fizz");
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 			printf(" Buzz");
 		else
 		       printf(" %d", i);	
