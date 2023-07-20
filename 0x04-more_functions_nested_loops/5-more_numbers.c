fa0fbf25bd8689c915998faa4183f42b4338954f
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 13:17:25 +0000

    cc

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers -lkdmfg
+ *
+ * Return: 14 numbers 10 times 
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+			putchar(j)
+		putchar('\n');	
+		j = 0
+	}
+}
