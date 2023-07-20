93ec6b6c7dd5c9fae78d802c9e8f88f4559436c4
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 10:10:39 +0000

    cc

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints whether a random integer is positive, negative, or zero
+ *
+ * Description: This function generates a random integer between
+ * -RAND_MAX/2 and RAND_MAX/2
+ * and prints whether it is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
