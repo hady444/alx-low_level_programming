da6e0b0acd2fedcb724a5994ea5c0605d62ab089
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 14:42:27 +0000

    cc

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,24 +2,25 @@
 /**
  * print_diagonal - fg
  * @n: fgg
- * Return: dsf
 */
 void print_diagonal(int n)
 {
-	if (n == 0)
-	{
-		_putchar('\n');
-		return;
-	}
 	int i, j;
 
-	for (i = 0 ; i < n ; i++)
+	if (n <= 0)
 	{
-		for (j = 0 ; j < i ; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1 ; i <= n ; i++)
+		{
+			for (j = 1 ; j <= i ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
