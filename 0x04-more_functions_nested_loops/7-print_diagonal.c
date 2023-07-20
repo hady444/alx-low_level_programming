bee4a58ab843e721861172708e480bcc493b2e04
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 14:05:26 +0000

    cc

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - fg
+ * @n: fgg
+ * Return: dsf
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < i ; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
