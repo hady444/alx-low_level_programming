67c10c58c990d38aa4fcc8c9192c34934014fad6
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 15:07:04 +0000

    cc

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * fizz_buzz - fdsg
+ * Return:void
+ *
+ */
+void fizz_buzz(void)
+{
+	printf("1");
+	for (i = 2 ; i <= 100 ; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else
+		       printf(" %d", i)	
+	}
+}
