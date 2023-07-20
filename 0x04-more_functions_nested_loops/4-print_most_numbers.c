2fc0b687575ff0f9cf3a38e3623f4548eefb4a5a
hady444 <hadyhassan2312@gmail.com.com>
2023-07-20 13:04:56 +0000

    cc

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - ldf
+ *
+ * Return: lmvf
+*/
+
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		if i == '4' || i == '2'{
+			i++;
+			continue
+		}
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
