2a4820f3a2e92a504f81f0aecb83452358381fe3
hady444 <hadyhassan2312@gmail.com.com>
2023-07-24 08:12:33 +0000

    c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - ,lfg
+ * print_rev - ,lfg
  * @s: dgf
  *
  */
@@ -11,5 +11,5 @@ void print_rev(char *s)
 
 	for (i = (len - 1) ; i >= 0 ; i--)
 		_putchar(s[i]);
-	_putchar('\n');	
+	_putchar('\n');
 }
