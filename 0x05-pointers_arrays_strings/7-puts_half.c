19f4ccfd6ee29c91b7e9c2498c88f6e3fa6dd16e
hady444 <hadyhassan2312@gmail.com.com>
2023-07-24 09:15:55 +0000

    c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - ,lfg
+ * @a: dgf
+ * @b: fggf
+ */
+void puts_half(char *str)
+{
+	int l = strlen(str);
+	int i;
+
+	for (i = l/2 ; i < l ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
