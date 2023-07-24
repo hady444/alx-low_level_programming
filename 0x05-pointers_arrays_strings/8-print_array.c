164024a2416a13f2536f8160c4d4d6b4fc05026e
hady444 <hadyhassan2312@gmail.com.com>
2023-07-24 09:32:47 +0000

    c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,14 +1,19 @@
 #include "main.h"
 /**
  *  print_array - ,lfg
- * @a:fdkf 
+ * @a:fdkf
  * @n: kfmsv
  */
 void print_array(int *a, int n)
 {
+	if (n <= 0)
+	{
+		printf('\n');
+		return;
+	}
 	int i;
 
-	for (i = 0 ; i < n-1 ; i++)
+	for (i = 0 ; i < n - 1 ; i++)
 		printf("%d, ", a[i]);
-	printf("%d\n", a[n-1]);
+	printf("%d\n", a[n - 1]);
 }
