b0cfc8b9e190f047b19be9fe01eefd10d030bba1
hady444 <hadyhassan2312@gmail.com.com>
2023-07-24 09:06:06 +0000

    c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,9 +8,12 @@ void rev_string(char *s)
 {
 	int len = strlen(s);
 	int i;
-	char *o;
+	char tmp;
 
-	for (i = (len - 1) ; i >= 0 ; i--)
-		strcat((s[i]),o);
-	*s = *o;
+	for (i = 0 ; i < len / 2 ; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len-i-1];
+		s[len-i-1] = tmp;
+	}
 }
