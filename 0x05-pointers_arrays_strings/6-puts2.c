b7cf35474e28e0228bbaaf794414ab923f0d82a1
hady444 <hadyhassan2312@gmail.com.com>
2023-07-24 09:12:30 +0000

    c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - ,lfg
+ * @str: dgf
+ *
+ */
+void puts2(char *str)
+{
+	int l = strlen(str);
+	int i;
+
+	for (i = 0 ; i < l ; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
