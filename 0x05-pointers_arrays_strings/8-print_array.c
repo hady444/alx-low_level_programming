8afbaa745885635cb9f4398a2ff53f8bd6d816bf
hady444 <hadyhassan2312@gmail.com.com>
2023-07-24 09:24:00 +0000

    c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - ,lfg
+ * @a:fdkf 
+ * @n: kfmsv
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n-1 ; i++)
+		printf("%d, ", a[i]);
+	printf(a[n-1]);
+}
