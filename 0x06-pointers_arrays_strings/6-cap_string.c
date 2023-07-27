6a067f1a04f01d8c71ddb15272be4aa4b5f94beb
hady444 <hadyhassan2312@gmail.com.com>
2023-07-27 19:16:56 +0000

    c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - okfd
+ * @str: fdg
+ * Return: fdf
+ *
+ */
+char *cap_string(char *str);
+{
+	int i = 0;
+	int j;
+	char del[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n'};
+
+	while (str[i + 1] != '\0')i
+	{
+		for (j = 0 ; j < 12 ; j++)
+		{
+			if (str[i] == del[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i] = str[i] - ('a' - 'A');
+				}
+				break;
+			}
+		}
+		i++;
+	return (str);
+}
