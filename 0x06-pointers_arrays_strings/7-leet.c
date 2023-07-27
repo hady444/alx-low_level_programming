0ac0e7e90e67f2af8b6d77e16cc83c1f4ee77b7d
hady444 <hadyhassan2312@gmail.com.com>
2023-07-27 20:26:03 +0000

    c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,26 +13,31 @@ char *leet(char *str)
 		if (str[i] == 'a' || str[i] == 'A')
 		{
 			str[i] = 4;
+			i++;
 			continue;
 		}
 		if (str[i] == 'e' || str[i] == 'E')
 		{
 			str[i] = 3;
+			i++;
 			continue;
 		}
 		if (str[i] == 'o' || str[i] == 'O')
 		{
 			str[i] = 0;
+			i++;
 			continue;
 		}
 		if (str[i] == 't' || str[i] == 'T')
 		{
 			str[i] = 7;
+			i++;
 			continue;
 		}
 		if (str[i] == 'l' || str[i] == 'L')
 		{
 			str[i] = 1;
+			i++;
 			continue;
 		}
 	}
