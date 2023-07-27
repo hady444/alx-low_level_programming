fc3c0312ccf5fc69c8ddd705d0f0c0b1cbc3e991
hady444 <hadyhassan2312@gmail.com.com>
2023-07-27 17:28:47 +0000

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 		i++;
-	while (src[j] != '\0' || (j <= (n / 8)))
+	while (src[j] != '\0' || (j <= (n)))
 	{
 		dest[i + j] = src[j];
 		j++;
