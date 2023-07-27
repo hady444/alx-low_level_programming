3b760639f6a2422f23549d3f7af2ff2e86561558
hady444 <hadyhassan2312@gmail.com.com>
2023-07-27 18:22:32 +0000

    c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,5 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return ((int) s1[0] - (int) s2[0]);
+	int i = 0;
+	int sum = 0;
+
+	while (s1[i] != 0)
+	{
+		if s1[i] != s2[i]{
+			sum += ((int) s1[i] - (int) s2[i]);
+			i++;
+		}
+	}
+	return (sum);
 }
