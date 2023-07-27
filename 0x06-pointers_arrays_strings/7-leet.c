31090278f9bd0bf5765dfa27c8764025d22ce4e9
hady444 <hadyhassan2312@gmail.com.com>
2023-07-27 20:18:44 +0000

    c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *leet - f,dlf
+ * @str: fgfg
+ * Return: gfg
+ */
+char *leet(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = 4;
+		else if (str[i] == 'e' || str[i] == 'E')
+                        str[i] = 3;
+		else if (str[i] == 'o' || str[i] == 'O')
+                        str[i] = 0;
+		else if (str[i] == 't' || str[i] == 'T')
+                        str[i] = 7;
+		else if (str[i] == 'l' || str[i] == 'L')
+                        str[i] = 1;
+	}
+	return (str);
+}
