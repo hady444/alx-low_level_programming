cbfaea5ea850a99da2b48b9616df0c94918bfb72
hady444 <hadyhassan2312@gmail.com.com>
2023-07-27 18:57:50 +0000

    c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,11 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
-		if (str[i] >= 'a')
+	while (str[i] != '\0') {
+		if (str[i] >= 'a' && str[i] <= 'z') {
 			str[i] = str[i] - ('a' - 'A');
+		}
 		i++;
-	return (str);
+	}
+    return str;
 }
