39171dab4e97192480426b69eb8a11a44e95aa19
hady444 <hadyhassan2312@gmail.com.com>
2023-07-27 18:33:28 +0000

    c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reverse_array - jijidf
+ * @a: fgdg
+ * @n: fdd
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = (n - 1) ; i > 0 ; i++)
+		printf("%d, ", a[i]);
+	printf("%d", a[i]);
+}
