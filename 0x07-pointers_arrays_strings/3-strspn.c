5c756ef478071ea773ef2b7160c09c8235712cde
hady444 <hadyhassan2312@gmail.com.com>
2023-08-01 20:39:27 +0000

    c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0' && accept[i] >= '\0' && s[i] == accept[i] ; i++)
+	for (i = 0; s[i] >= '\0' && accept[i] >= '\0' && s[i] == accept[i] ; i++)
 	{}
 	return (i);
 }
