5f0b14cb1e4bc4452d2c02c94364a13bd9038dc8
hady444 <hadyhassan2312@gmail.com.com>
2023-08-01 20:21:59 +0000

    c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr: find first occurance of input char
+ * @c: input character to be find
+ * Return: first occurance index of character c
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		if s[i] == c
+			return (s + i);
+	}
+	return (Null);
+}
