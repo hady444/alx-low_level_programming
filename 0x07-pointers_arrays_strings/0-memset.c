73f948388637d338db7ff16e6a375faa8e4fe718
hady444 <hadyhassan2312@gmail.com.com>
2023-08-01 20:02:12 +0000

    c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - dfds
+ * @s: pointer to first element in buffer
+ * @b: value to be set
+ * @n: number of bytes to be set
+ * Return: cfg
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		*(s+i) = b;
+	return *s;
+}
