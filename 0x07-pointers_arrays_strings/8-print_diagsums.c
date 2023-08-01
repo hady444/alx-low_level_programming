de8f6f4223c7d3e88d5d3fbc1e264995f300de2f
hady444 <hadyhassan2312@gmail.com.com>
2023-08-01 22:58:50 +0000

    c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,11 +6,14 @@
  */
 void print_diagsums(int *a, int size);
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < size; i++)
 	{
 		_putchar(a[i][i]);
 	}
-	_putchar('\n');
+	_putchar(',');
+	_putchar(' ');
+	for (i = size - 1, j = 0; i < size; i--, j++)
+		_putchar(a[i][j]);
 }
