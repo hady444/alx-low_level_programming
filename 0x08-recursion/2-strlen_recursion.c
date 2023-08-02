10d738217fcd050318ed278d580933def98fce85
hady444 <hadyhassan2312@gmail.com.com>
2023-08-02 22:24:23 +0000

    c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen_recursion - jdfmg 
+ * _strlen_recursion - jdfmg
  * @s: fggf
  * Return: nkndfg
  */
@@ -9,5 +9,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
