5bff70453afd53d2942ff34ae46b1733ebfb81eb
hady444 <hadyhassan2312@gmail.com.com>
2023-08-03 11:32:56 +0000

    x

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,10 +4,6 @@
  * @n: gfgfmk
  * Return: gfg
  */
-int _sqrt_recursion(int n)
-{
-	return (sqr(n, 0));
-}
 int sqr(int n, int val)
 {
 	if (val * val == n)
@@ -16,4 +12,8 @@ int sqr(int n, int val)
 		return (sqr(n, val + 1));
 	else
 		return (-1);
+}
+int _sqrt_recursion(int n)
+{
+        return (sqr(n, 0));
 }
