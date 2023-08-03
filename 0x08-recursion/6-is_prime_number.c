f2cbd7af673f5796beda98dcd3eb8c70dae21ba1
hady444 <hadyhassan2312@gmail.com.com>
2023-08-03 11:51:43 +0000

    x

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
  * is_prime_number - fhfh
- *@n: gfg
+ * @n: gfg
+ * @np: fgg
  * Return: fgfg
  */
 int check_prime(int n, int np);
 int is_prime_number(int n)
 {
-	return (check_prime(n, 1));
+	return (check_prime(n, 2));
 }
 /**
  * check_prime - fgg
  * @n: fgf
- * @np: gff
+ * @np: gfdf
  * Return: fgfg
  */
 int check_prime(int n, int np)
 {
 	if (np >= n && n > 1)
 		return (1);
-	else if (n % np == 0 || n < 0)
+	else if (n % np == 0 || n <= 0)
 		return (0);
 	else
 		return (check_prime(n, np + 1));
