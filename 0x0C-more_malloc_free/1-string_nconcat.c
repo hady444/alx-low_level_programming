f85ed2108fed4db7b28766583df864144726d26d
hady444 <hadyhassan2312@gmail.com.com>
2023-08-10 22:54:03 +0000

    c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,5 @@
 #include "main.h"
 /**
- * _min - d
- * @a: 1s
- * @b: nd
- * Return: minimum
- */
-int _min(unsigned int a, unsigned int b)
-{
-	if (a > b)
-		return (b);
-	return (a);
-}
-/**
  * string_nconcat - fg
  * @s1: fgh
  * @s2: fkgf
@@ -20,29 +8,41 @@ int _min(unsigned int a, unsigned int b)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s = NULL;
-	unsigned int i;
-	unsigned int j;
-	unsigned int i1;
-	unsigned int j1;
+	char *strnew = NULL;
+	unsigned int i, n1, n2, j, count, palabras;
 
-	i = 0;
-	i1 = 0;
+	count = 0;
+	palabras = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (; s1[i] != '\0' ; i++)
-	{}
-	for (; s2[i1] != '\0' ; i1++)
-	{}
-	s = malloc((i + i1) * sizeof(char) + 1);
-	if (s == NULL)
+	for (n1 = 0; s1[n1] != '\0'; n1++)
+		;
+	for (n2 = 0; s2[n2] != '\0'; n2++)
+		;
+	if (n >= n2)
+	{
+		palabras = n2;
+
+	} else
+	{
+		for (n2 = 0; n2 < n; n2++)
+			palabras++;
+	}
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+	if (strnew == NULL)
+	{
 		return (NULL);
-	for (j = 0; j < i; j++)
-		s[j] = s1[j];
-	for (j1 = 0; j1 < _min(i1, n); j1++)
-		s[i + j1] = s2[j1];
-	s[i + i1] = '\0';
-	return (s);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (j = 0; j < palabras; i++)
+	{
+		strnew[i] = s2[count];
+		count++;
+		j++;
+	}
+	strnew[i] = '\0';
+	return (strnew);
 }
