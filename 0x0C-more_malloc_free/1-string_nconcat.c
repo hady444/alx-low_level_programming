da3f4e5355b225e24abb70a38be2b7581b8fa7e6
hady444 <hadyhassan2312@gmail.com.com>
2023-08-10 22:32:57 +0000

    cc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _min - d
+ * @a: 1s
+ * @b: nd
+ * Return: minimum
+ */
+int _min(int a, int b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+/**
+ * string_nconcat - fg
+ * @s1: fgh
+ * @s2: fkgf
+ * @n: max length
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	char *s = NULL;
+	int i;
+	int j;
+	int i1;
+	int j1;
+
+	i = 0;
+	i1 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (; s1[i] != '\0' ; i++)
+	{}
+	for (; s2[i1] != '\0' ; i1++)
+	{}
+	s = malloc((i + i1) * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		s[j] = s1[j];
+	for (j1 = 0; j1 < _min(i1, n); j1++)
+		s[i + j1] = s2[j1];
+	s[i + i1] = '\0';
+	return (s);
+}
