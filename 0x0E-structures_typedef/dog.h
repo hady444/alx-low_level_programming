6f67abff1fc7ea62618739fcfcda87c7096af2ce
hady444 <hadyhassan2312@gmail.com.com>
2023-08-14 17:22:01 +0000

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new defined structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner name
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
