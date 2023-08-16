110f3a1813787f47c1fd9ecb945cfa68ec009d0e
hady444 <hadyhassan2312@gmail.com.com>
2023-08-16 18:31:54 +0000

    third task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,27 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
+ * int_index - dsa
+ * @array: pointer to array
+ * @size: size of array
+ * @cmp: pointer to fun
+ * Return: int
  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		i = 0;
+		for (; i < size; i++)
+		{
+			if ((*cmp)(array[i]) != 0)
+				return (i);
+		}
+		return (-1);
+	}
+	return (-1);
+}
+
