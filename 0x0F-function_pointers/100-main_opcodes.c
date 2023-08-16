6768e5a0706868df8f3811ad9bf88fd0cfbe3fce
hady444 <hadyhassan2312@gmail.com.com>
2023-08-16 21:03:46 +0000

    fifth task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,5 @@
-#include "function_pointers.h"
+#include<stdio.h>
+#include<stdlib.h>
 /**
  * main- Entry point
 (* a blank line
@@ -9,22 +10,25 @@
 */
 int main(int argc, char *argv[])
 {
-	int bytes = atoi(argv[1]), i;
+	int i, n;
+
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	if (bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < bytes; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		printf("%02hhx", *((char *)main + i));
-		if (i < bytes - 1)
+		if (i < n - 1)
 			printf(" ");
 		else
 			printf("\n");
