ce60c50c0a9dbffe192eb336d15423b425353fea
hady444 <hadyhassan2312@gmail.com.com>
2023-08-16 18:18:00 +0000

    second task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,8 +9,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
+	if (array && size && action)
+	{
 	for (; i < size ; i++)
 	{
 		(*action)(array[i]);
+	}
 	}
 }
