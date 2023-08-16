6667ced0a2b9644e6ea2ddcab7875f8e3b5b2d4a
hady444 <hadyhassan2312@gmail.com.com>
2023-08-16 18:13:09 +0000

    second task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,16 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
+ * array_iterator - fdf
+ * @array: pointer to array
+ * @size: size of array
+ * @action: pointer to function
  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	 long unsigned int i = 0;
+
+	for (; i < size ; i++)
+	{
+		(*action)(array[i]);
+	}
+}
