cf26a60f2c6ff244789e4b0a1d3d87b72917a268
hady444 <hadyhassan2312@gmail.com.com>
2023-08-16 20:51:25 +0000

    fifth task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,3 +4,27 @@
  *
  *
  */
+int main(int argc, char *argv[])
+{
+	int bytes = atoi(argv[1]), i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
