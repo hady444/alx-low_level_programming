edca7178aa23a0300b48e3ab225147b8390410be
hady444 <hadyhassan2312@gmail.com.com>
2023-08-17 22:55:45 +0000

    second task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,15 +6,20 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list ptr;
+	char *sep;
 	unsigned int i;
+	va_list ptr;
 
+	if (separator == NULL || *separator == 0)
+		sep = "";
+	else
+		sep = (char *) separator;
 	va_start(ptr, n);
 	if (n != 0)
 		printf("%d", va_arg(ptr, int));
 	for (i = 1; i < n; i++)
 	{
-		printf("%s%d", separator, va_arg(ptr, int));
+		printf("%s%d", sep, va_arg(ptr, int));
 	}
 	printf("\n");
 	va_end(ptr);
