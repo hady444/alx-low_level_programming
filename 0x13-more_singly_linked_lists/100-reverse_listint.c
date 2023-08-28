5ebe96ebb92a938f7b462b6ccc8c05f5b1c47480
hady444 <hadyhassan2312@gmail.com.com>
2023-08-28 15:22:23 +0000

    c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 /**
- * -
- * @:
- * Return:
+ * reverse_listint - reverse list
+ * @head: pointer to head
+ * Return: revesed list
  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp1, *tmp2;
+
+	tmp2 = (*head)->next;
+	while (tmp2 != NULL)
+	{
+		tmp1 = *head;
+		*head = tmp2;
+		tmp2 = tmp2->next;
+		(*head)->next = tmp1;
+	}
+	return (*head);
+}
