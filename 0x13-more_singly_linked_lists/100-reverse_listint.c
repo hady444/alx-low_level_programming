625f22cdf3dc06488d5a659a7e477a41d7dc3ff3
hady444 <hadyhassan2312@gmail.com.com>
2023-08-28 17:08:15 +0000

    c100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,13 +8,16 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *tmp1, *tmp2;
 
-	tmp2 = (*head)->next;
-	while (tmp2 != NULL)
+	if (!*head || !head)
+		return (NULL);
+	tmp1 = NULL;
+	while ((*head)->next != NULL)
 	{
+		tmp2 = (*head)->next;
+		(*head)->next = tmp1;
 		tmp1 = *head;
 		*head = tmp2;
-		tmp2 = tmp2->next;
-		(*head)->next = tmp1;
 	}
+	(*head)->next = tmp1;
 	return (*head);
 }
