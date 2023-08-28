09543bbbe9ba1894caad3597e490964390f498c9
hady444 <hadyhassan2312@gmail.com.com>
2023-08-28 09:16:53 +0000

    c1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,17 @@
 #include "lists.h"
 /**
- * -
- * @:
- * Return:
+ * listint_len - return list length
+ * @h: list
+ * Return: length of list
  */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
