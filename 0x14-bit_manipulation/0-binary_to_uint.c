4d5be397ca526f7aa07371559a4104faf0a8895b
hady444 <hadyhassan2312@gmail.com.com>
2023-09-28 13:39:44 +0000

    binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = strlen(b) - 1, sum = 0, count = 0, x, incrementor = 1;
+
+	if (!b)
+		return (0);
+
+	for (; i > -1; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+		{
+			if (count == 0)
+			{
+				sum += 1;
+				count++;
+				continue;
+			}
+			x = count;
+			do {
+				incrementor *= 2;
+				x--;
+			} while (x > 0);
+			sum += incrementor;
+			incrementor = 1;
+		}
+		count++;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This bit manipulation day!!
