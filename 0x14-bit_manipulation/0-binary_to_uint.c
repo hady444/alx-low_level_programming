edf7fd6a968982c05bccbe588b91cfcbc70f19bb
hady444 <hadyhassan2312@gmail.com.com>
2023-09-28 13:53:58 +0000

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (; i > -1; i--)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
 		{
