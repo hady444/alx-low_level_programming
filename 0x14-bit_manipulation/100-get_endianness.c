0df36fe558acd8c942bfd1bd634bc867edfacadb
hady444 <hadyhassan2312@gmail.com.com>
2023-09-28 18:45:36 +0000

    task 7

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,11 @@
 /**
- * get_endianness - knoe of OS 
+ * get_endianness - knoe of OS
+ *
  * Return: 0 big , 1 little
  */
 int get_endianness(void)
 {
-        unsigned int m = 1;
+	unsigned int m = 1;
 
-        return (*(char *)(&m) == 1);
+	return (*(char *)(&m) == 1);
 }
