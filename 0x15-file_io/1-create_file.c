f8ba2d7cbbea4c918b33296e5197d8fc54986a87
hady444 <hadyhassan2312@gmail.com.com>
2023-10-02 20:06:01 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - a function creates a file
+ * @filename: file path or name
+ * @text_content: content of file
+ * Return: return 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = open(filename, O_RDWR | O_CREAT, 0600);
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, strlen(text_content));
+	close(fd);
+	return (1);
+}
