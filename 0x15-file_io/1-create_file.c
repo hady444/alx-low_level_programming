a7a5aca417255d99ecc8b3efe389e6785c635608
hady444 <hadyhassan2312@gmail.com.com>
2023-10-02 20:08:27 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,15 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_RDWR | O_CREAT, 0600);
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 	write(fd, text_content, strlen(text_content));
 	close(fd);
 	return (1);
