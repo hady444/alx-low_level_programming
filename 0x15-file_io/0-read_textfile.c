46f5967b1e62e3a283debb6b73a9db9eeb957108
hady444 <hadyhassan2312@gmail.com.com>
2023-10-02 19:52:31 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,14 +10,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t file = open(filename, O_RDONLY);
 	ssize_t r, w;
 	char *c;
-	
+
 	if (file == -1)
 	{
 		return (0);
 	}
 	c = malloc(sizeof(char) * letters);
 	if (!c)
-                return (0);
+		return (0);
 	r = read(file, c, letters);
 	w = write(STDOUT_FILENO, c, r);
 	if (w == -1)
