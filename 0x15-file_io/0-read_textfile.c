214464e67c4b363a2b30b61f2ec759191703051c
hady444 <hadyhassan2312@gmail.com.com>
2023-10-02 19:18:52 +0000

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textfile - read a file and prints it to POSIX standard output.
+ * @filename: file
+ * @letters: number of read chars
+ * Return: actual number it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file = fopen(filename, "r");
+	ssize_t i;
+	char c;
+
+	if (file == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; letters > 0; i++, letters--)
+	{
+		if ((c = getc(file)) == EOF)
+			break;
+		printf("%c", c);
+	}
+	fclose(file);
+	return (i);
+}
