b8c5a8092a1e7df32a207aa49261248e2a8947e0
hady444 <hadyhassan2312@gmail.com.com>
2023-11-09 18:27:29 +0000

    c1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - print elements in linked list
+ * @h: pointer to list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *travers;
+	size_t s = 0;
+
+	travers = h;
+	while (travers)
+	{
+		travers = travers->next;
+		s++;
+	}
+	return (s);
+}
