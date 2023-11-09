9354da319a38be62719f8e9d9fde50208bb13ee9
hady444 <hadyhassan2312@gmail.com.com>
2023-11-09 21:39:52 +0000

    c7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,53 +1,46 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - inserts a node node at a given position
- * in a dlistint_t list.
- * @h: pointer to the list.
- * @idx: position to add the node.
- * @n: data for the new node.
- * Return: the address of the new node, or NULL if it failed
- **/
+ * insert_dnodeint_at_index - insert
+ * @h: pointer to head
+ * @idx: specified index
+ * @n: number reserved in node
+ * Return: list
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux_node = *h, *new_node;
-	unsigned int index, cont = 0;
+	dlistint_t *node, *tra;
+	unsigned int i = 0;
 
-	/* create node */
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (h == NULL)
 		return (NULL);
-	new_node->n = n;
-
-	/* border case for insert at the beginning */
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->next = *h;
-		if (*h)
-			(*h)->prev = new_node;
-		*h = new_node;
+		node->prev = NULL;
+		node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = node;
+		*h = node;
 		return (*h);
 	}
-
-	/* search of position to insert */
-	index = idx - 1;
-	while (aux_node && cont != index)
+	tra = *h;
+	while (tra && i != idx - 1)
 	{
-		cont++;
-		aux_node = aux_node->next;
+		tra = tra->next;
+		i++;
 	}
-
-	/* general case */
-	if (cont == index && aux_node)
+	if (i == idx - 1 && tra)
 	{
-		new_node->prev = aux_node;
-		new_node->next = aux_node->next;
-		if (aux_node->next)
-			aux_node->next->prev = new_node;
-		aux_node->next = new_node;
-		return (new_node);
+		node->prev = tra;
+		node->next = tra->next;
+		if (tra->next)
+			tra->next->prev = node;
+		tra->next = node;
+		return (*h);
 	}
-	free(new_node);
-	return (NULL);
+	free(node);
+	return (*h);
 }
