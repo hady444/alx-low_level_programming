5bd17d9d2ac39b3f46eb548feff9dc99bb7c9a9f
hady444 <hadyhassan2312@gmail.com.com>
2023-11-09 18:43:10 +0000

    c2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,9 +10,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *node;
 
 	node = malloc(sizeof(dlistint_t *));
+	if (!node)
+		return (NULL);
 	node->prev = NULL;
 	node->n = n;
 	node->next = *head;
+	if (head != NULL)
+		*head->prev = node;
 	*head = node;
 	return (*head);
 }
