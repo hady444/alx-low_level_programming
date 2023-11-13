40502583e422fa03a93b2609ec5a368b0774b0f8
hady444 <hadyhassan2312@gmail.com.com>
2023-11-13 22:40:34 +0000

    g

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/hady444/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P .. https://github.com/hady444/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
