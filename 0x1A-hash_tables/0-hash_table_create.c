0621c33c083d208435c6adbe9977612b836e61d0
hady444 <hadyhassan2312@gmail.com.com>
2023-12-02 00:43:22 +0000

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,9 +7,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table;
+	unsigned long int i;
 
 	h_table = malloc(sizeof(hash_table_t) * size);
 	if (!h_table)
 		return (NULL);
+	h_table->size = size;
+	h_table->array = malloc(sizeof(hash_node_t *));
+	if (!(h_table->array))
+	{
+		free(h_table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		h_table->array[i] = NULL;
 	return (h_table);
 }
