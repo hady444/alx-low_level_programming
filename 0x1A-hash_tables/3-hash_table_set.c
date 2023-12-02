d664e38f90f5469e8d98203b0450bbf25a6ccfd2
hady444 <hadyhassan2312@gmail.com.com>
2023-12-02 02:15:38 +0000

    2

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
-                return (0);
+		return (0);
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
@@ -40,11 +40,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
-        {
-                free(new_node);
+	{
+		free(new_node);
 		free(new_node->key);
-                return (0);
-        }
+		return (0);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
