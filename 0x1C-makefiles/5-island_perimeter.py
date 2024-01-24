1aeceeeac7d710641caacbf8a1ba686f834e1649
hady444 <hadyhassan2312@gmail.com.com>
2024-01-24 22:32:05 +0000

    task4,5,100

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def island_perimeter(grid):
+    """
+    Function to get the perimeter of the island described in grid.
+    
+    Attributes:
+        grid:  is a list of list of integers
+    Return: perimeter of the island
+    """
+    max_rows = 0
+    found = 0
+    for row in grid:
+        rr = row.count(1)
+        if rr > 0:
+            found += 1
+        if max_rows < rr:
+            max_rows = rr
+    return ((max_rows + found) * 2)
